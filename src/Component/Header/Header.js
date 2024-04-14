b3f3209871d137d11d1d878f3b4b1d747b305099
Aman Nihaal Nuckchady <amannuckchady@gmail.com>
2024-04-14 12:49:34 -0400

    navbar and card

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Header.css";
 import logo from "../Assets/shopping-location-icon-vector.jpg";
-import { BiSearchAlt2, BiUser } from "react-icons/bi";
+import { BiSearchAlt2, BiUser, BiCart } from "react-icons/bi";
 import { AiOutlineMenu } from "react-icons/ai";
+
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="logo-search-container gap-3">
-        <AiOutlineMenu className="h-5 w-auto" />
+        <AiOutlineMenu className="h-6 w-6" />
         <div className="logo-cont">
           <img src={logo} alt="logo" />
           <h1 className="text-lg font-semibold">DiscountFinder</h1>
         </div>
 
         <div className="search-container">
-          <BiSearchAlt2 />
+          <BiSearchAlt2/>
           <input type="text" placeholder="Enter a product" />
         </div>
 
@@ -23,6 +24,10 @@ const Header = () => {
           <h1>Username</h1> {/* add username here*/}
           <div className="headerIcon">
             <BiUser className="w-5 h-5"/>
+          </div>
+
+          <div className="headerIcon">
+            <BiCart className="w-5 h-5"/>
           </div>
         </div>
       </div>
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import Navbar from "../Navbar/Navbar";
 const Home = () => {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   );
 };

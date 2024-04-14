f73a2e8541ddb9965db9244d7bea0456f7a6a65c
Aman Nihaal Nuckchady <amannuckchady@gmail.com>
2024-04-14 09:35:43 -0400

    profile icon added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Header.css";
 import logo from "../Assets/shopping-location-icon-vector.jpg";
-import { BiSearchAlt2 } from "react-icons/bi";
+import { BiSearchAlt2, BiUser } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="logo-search-container gap-3">
+        <AiOutlineMenu className="h-5 w-auto" />
         <div className="logo-cont">
           <img src={logo} alt="logo" />
           <h1 className="text-lg font-semibold">DiscountFinder</h1>
@@ -15,6 +17,12 @@ const Header = () => {
         <div className="search-container">
           <BiSearchAlt2 />
           <input type="text" placeholder="Enter a product" />
+        </div>
+
+        <div className="profile">
+          <div className="headerIcon">
+            <BiUser className="w-5 h-5"/>
+          </div>
         </div>
       </div>
     </div>

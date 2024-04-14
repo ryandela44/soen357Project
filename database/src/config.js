c590395976a02fc243975a253ea9b5cbd4b12d29
Ryan Wetie <67614966+ryandela44@users.noreply.github.com>
2024-04-14 15:03:20 -0400

    updated packages

--- database/src/config.js
+++ database/src/config.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/storeDatabase', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected successfully.');
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+const connectToDatabase = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/database', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.error('MongoDB connection error:', err));
+};
+
+module.exports = connectToDatabase;

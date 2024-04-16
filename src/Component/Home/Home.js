b4ec00af9312f0b4292785cc1550b9651d7def4d
Ryan Wetie <67614966+ryandela44@users.noreply.github.com>
2024-04-16 19:06:59 -0400

    fixed carousel

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -22,20 +22,26 @@ const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
+    autoplay:true,
+    autoplaySpeed: 7000,
     slidesToScroll: 1,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 2,
+                autoplay: true,
+                autoplaySpeed: 7000
             }
         },
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 7000
             }
         }
     ]

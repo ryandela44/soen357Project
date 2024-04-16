094fb16ec2f4e9a13c366112f0e63304d53dbf1e
Ryan Wetie <67614966+ryandela44@users.noreply.github.com>
2024-04-16 00:46:38 -0400

    attempt to reactive header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,72 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../Assets/shopping-location-icon-vector.jpg";
 import { BiSearchAlt2, BiUser, BiCart } from "react-icons/bi";
 import { AiOutlineMenu } from "react-icons/ai";
 import { useUser } from '../UserContext/UserContext';
 
-
 const Header = () => {
-  const { user } = useUser();
-  return (
-    <div className="header-container">
-      <div className="logo-search-container gap-3">
-      <div className="headerMenuIcon">
-        <AiOutlineMenu className="h-6 w-6" />
-        </div>
-        <div className="logo-cont">
-          <img src={logo} alt="logo" />
-          <h1 className="text-lg font-semibold">DiscountFinder</h1>
-        </div>
+    const { user } = useUser();
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
-        <div className="search-container">
-          <BiSearchAlt2/>
-          <input type="text" placeholder="Enter a product" />
-        </div>
+    const toggleDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    };
 
-        <div className="navbar">
-          <ul>
-            <li>
-              <div className="list-cont">
-                <a href="#">Home</a>
-              </div>
-            </li>
-            <li>
-              <div className="list-cont">
-                <a href="#">Shops</a>
-              </div>
-            </li>
-            <li>
-              <div className="list-cont">
-                <a href="#">Discounts</a>
-              </div>
-            </li>
-            <li>
-              <div className="list-cont">
-                <a href="#">Blog</a>
-              </div>
-            </li>
-            <li>
-              <div className="list-cont">
-                <a href="#">Contact</a>
-              </div>
-            </li>
-          </ul>
-        </div>
+    return (
+        <div className="header-container">
+            <div className="logo-search-container gap-3">
+                <div className="headerMenuIcon" onClick={toggleDrawer}>
+                    <AiOutlineMenu className="h-6 w-6" />
+                </div>
+                <div className="logo-cont">
+                    <img src={logo} alt="logo" />
+                    <h1 className="text-lg font-semibold">DiscountFinder</h1>
+                </div>
 
-        <div className="profile">
-          <h1>{user ? user.email : 'Username'}</h1> {/* Display user email or default text */}
-          <div className="headerIcon">
-            <BiUser className="w-5 h-5"/>
-          </div>
+                <div className={`search-container ${drawerOpen ? 'hide' : ''}`}>
+                    <BiSearchAlt2/>
+                    <input type="text" placeholder="Enter a product" />
+                </div>
 
-          <div className="headerIcon">
-            <BiCart className="w-5 h-5"/>
-          </div>
+                <div className={`navbar ${drawerOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <div className="list-cont">
+                                <a href="#">Home</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-cont">
+                                <a href="#">Shops</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-cont">
+                                <a href="#">Discounts</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-cont">
+                                <a href="#">Blog</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-cont">
+                                <a href="#">Contact</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="profile">
+                    <h1>{user ? user.email : 'Username'}</h1> {/* Display user email or default text */}
+                    <div className="headerIcon">
+                        <BiUser className="w-5 h-5"/>
+                    </div>
+
+                    <div className="headerIcon">
+                        <BiCart className="w-5 h-5"/>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
